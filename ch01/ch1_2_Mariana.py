d69a907ddced5e917cf5babccbef1f2baec8bbcc
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-11-28 16:52:45 +0000

    added more commands in ch1_2_Mariana.py

--- ch01/ch1_2_Mariana.py
+++ ch01/ch1_2_Mariana.py
@@ -8,3 +8,19 @@ Created on Wed Nov 28 16:38:50 2018
 a=1
 a=a+1
 print(a)
+b="hello"
+print(b)
+c=b.title()
+print(b)
+print(c)
+d="hello"
+e=d.title()
+print(d)
+print(e)
+name="Dave"
+f="Hello {0}!".format(name)
+print(f)
+name="Sarah"
+print(f)
+print(name)
+print(name*5)
