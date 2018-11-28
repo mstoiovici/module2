2498b1df5cfe05cbee46d0618837777a530be17d
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-11-28 16:10:46 +0000

    initial commit

---
+++ ch01/ch1_Mariana.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 14:30:06 2018
+
+@author: maria
+"""
+
+A=2 + 3
+B=2.9 + 3.2
+C=(3.2 / 2.0) + 7
+D=2**3
+E=4**(1/2)
+F=str(6) + "a"
+print(A)
+print(B)
---
+++ ch01/hello.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello World!")
+print (5-6)
+print(2+5)
+2 + 3
+2.9 + 3.2
+(3.2 / 2.0) + 7
+2**3
+4**(1/2)
+str(6) + "a"
+
