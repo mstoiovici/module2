9418ff0c0be11bf5952583f91d07257dd3311095
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-11-28 17:06:08 +0000

    run commands in ex1.py

--- ch01/ex1.py
+++ ch01/ex1.py
@@ -5,3 +5,10 @@ Created on Wed Nov 28 16:56:28 2018
 @author: maria
 """
 
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print("Yay!Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
