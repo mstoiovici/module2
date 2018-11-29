17bbd0828066509590e1eb77408912b398efc60e
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-11-29 15:39:10 +0000

    added ch03 and ch3_Mariana.py file

---
+++ ch03/ch3_Mariana.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 13:59:18 2018
+
+@author: maria
+"""
+#print("What is your name?")
+#name=input().upper()
+#name=name.upper()
+#print("hello {}".format(name.upper()))
+
+
+
+
+   
+def hello_world():
+    print("Hello World!")
+    print_name()
+def print_name():
+    print("Type your name")
+    name=input()
+    sum=2+2
+    print("Nice to meet you "+name+"\n"+str(sum))    
+hello_world()   
+
+    
