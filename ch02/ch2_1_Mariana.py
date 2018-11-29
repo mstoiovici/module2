ca3a4a7542611626a356980e26dce4815e6dc5b6
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-11-29 11:14:03 +0000

    more commands

--- ch02/ch2_1_Mariana.py
+++ ch02/ch2_1_Mariana.py
@@ -1,4 +1,5 @@
 print(5-6)
+print("\n")
 print(8*9)
 print(6/2)
 print(5/2)
@@ -6,3 +7,37 @@ print(5.0 / 2)
 print(5 % 2)
 print(2 * (10 + 3))
 print(2 ** 4)
+age = 5
+age = "almost three"
+age = 29.5
+age = "I really don’t know!"
+print(age)
+print ("hello" + "world")
+print ("Joke " * 3)
+print ("Chen" + "3")
+a="Chen" + "3"
+print(a)
+type(a)
+type("Chen" + "3")
+print ("hello".upper())
+S1 = "hello" + "world"
+S2 = "Joke " * 3
+S3 = "5"
+print((S1 + S2)*10)
+print(S1+S2+S3)
+print("Bob \n" *10)
+print("bob" + "\n")
+print("\n")
+print("bob")
+print("john")
+print("john"+"\n")
+print("bob")
+s1="4"
+s2="5"
+s3=4
+result=int(s1)+int(s2)+s3
+#print(result)
+print(result)
+strExample="a,b,c,d,happy"
+splitExample=strExample.split("-")
+print(splitExample)
