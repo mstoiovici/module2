1eaafd3081538173d185d2853da77dab1848190d
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-04 16:59:02 +0000

    conditionals and extra work on currency file

--- ch03/currency.py
+++ ch03/currency.py
@@ -5,17 +5,22 @@ Created on Mon Dec  3 15:04:42 2018
 
 @author: maria
 """
-
+def condition(currency):
+    if currency=="romanian":
+        print("Those money converts into {} romanian lei ".format(romanian))
+        
 def change_currency(money):
-    romanian_value= money * 5.15
-    american_value=money*0.85
-    euro_value=money*0.90
-    values=(romanian_value,american_value,euro_value)
-    print("Those money converts into {} romanian lei ".format(romanian_value))
-    print("Or you could get {} american dollars ".format(american_value))
-    print("Or  {} euros, if you want to travel to Europe ".format(euro_value))
+    romanian= money * 5.15
+    american=money*0.85
+    euro=money*0.90
+    values=(romanian,american,euro)
+    #print("Those money converts into {} romanian lei ".format(romanian))
+    print("Or you could get {} american dollars ".format(american))
+    print("Or  {} euros, if you want to travel to Europe ".format(euro))
     return values
 
-money=float(input("Hello,how many pounds would you like to convert?"))
+money=float(input("Hello,how many pounds would you like to convert? "))
+currency=input("And what currency would you prefer? You can choose from romanian, american or euros " )
+
 change_currency(money)
 print("Now, tell me") # have to imprve this code with more statement, operations, or conditionals
--- ch4_Mariana.py
+++ ch4_Mariana.py
@@ -5,4 +5,55 @@ Created on Tue Dec  4 09:05:55 2018
 
 @author: maria
 """
+"""
+age=15
+isaTeen=age>=13 and age<=19
+print(isaTeen)
 
+age=22
+isaTeen=age>=13 and age<=19
+print(isaTeen)
+"""
+"""
+number=input("enter a number between 1 and 10: ")
+number=int(number)
+if number>10:
+    print("Too high!")
+elif number<=0:
+    print("Too low!")
+#else:
+    #print("That's perfect. Your number is between 1 and 10!")
+if 0<number<=10:
+    print("That's perfect. Your number is between 1 and 10!")
+"""
+"""
+age=int(input("please enter age number: "))
+if age<13:
+    print("child")
+elif age<18:
+    print("teen")
+elif age<65:
+    print("adult")
+elif age<100:
+    print("pensioner")
+else:
+    print("Are you sure you're still alive?!")
+"""
+
+######################ex11 LPTHW
+"""
+print("How old are you?"),
+age=input()
+print("How tall are you?"),
+height=input()
+print("How much do you weigh?"),
+weight=input()
+print (("So, you're %r tall and %r heavy.")%(age, height,weight))
+"""
+
+age=raw_input("How old are you?")
+
+height=raw_input("How tall are you?")
+
+weight=raw_input("How much do you weigh?")
+print (("So, you're %r tall and %r heavy.")%(age, height,weight))
