dbe7a7d2896dc64a197ea592a349cc3aead5baba
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-05 16:58:41 +0000

    added ch5_Mariana with problems in classes, objects, inheritance

---
+++ ch05/ch5_Mariana.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 09:51:49 2018
+
+@author: maria
+"""
+"""
+#Task1
+class Customer(object):
+    def __init__(self,name,balance=0.0):
+        #return a customer object whose name is "name" and starting balance is "balance" 
+        self.name=name
+        self.balance=balance
+    def withdraw(self,amount):
+        #return the balance remaining after withdrawing "amount" dollars 
+        if amount>self.balance:
+            raise RuntimeError("Amount greater than available balance.")
+        self.balance-=amount
+        return self.balance
+    def deposit(self, amount):
+        #Return the balance remaining after depositing *amount* dollars.
+        self.balance += amount
+        return self.balance
+
+#now we can create customers/objects of customers
+        
+jason=Customer("Jason Taylor",1000.0)
+print(jason.name)
+"""
+
+
+#Task2 #####ONE DIRECTION INHERITANCE
+class Animal():
+    def eat(self):             #superclass method
+        print('yum')
+
+class Dog(Animal):          
+     def bark(self):          #subclass method
+         print('Woof!')
+
+class Cat(Dog):               # the Cat class inherites directly from Dog class, so you can call the Bark()method, but than again, Dog class inherites from Animal  so Cat becomes grandchild of Animal class, and so can call the Eat()method
+    def meow(self):         #subclass method
+         print('Meow')
+
+Snoopy=Dog()            # creating an object Snoopy with the subclass Dog and class Animal
+#Snoopy.bark()              #subclass method
+#Snoopy.eat()               #superclass method
+
+Pissy=Cat()             # creating an object Pissy with the subclass Cat and class Animal
+#Pissy.meow()           #subclass method
+#Pissy.eat()             #superclass method
+
+
+#Task3
+class Robot():
+    def __init__(self,name,hours=0):
+        self.name=name
+        self.hours=hours
+    def move(self):
+        print('...move move move...')
+class CleanRobot(Robot):
+    def clean(self):
+        print('I vacuumed all the dust today')
+    def clean_bathroom(self,time):
+        print("I cleaned the toilet today....yuuuuk lady!!!")
+        if time>11:
+            print("I emptied the  bathroom trash bin as well")
+        elif time>6:
+            print ("I didn't empty the bathroom trash bin yet, it's too early. I don't want to do that task twice this evening lady!!!")
+        else:
+            print("And I was too lazy to check the bathroom trash bin.")
+class CookRobot(CleanRobot):
+    #def __init__(self,hours=0):
+        #self.hours=hours
+    
+    def cook(self,time):
+        if time>11:
+            print("It's too late to have dinner, I made you only a desert. I know you can't go to sleep without dessert!!!")
+            self.clean()
+        elif time>6:
+            print ('I made rice.I hope that"s enough for you')
+        else:
+            print("I didn't cook. I'm not your robot woman!!!!!")
+            print("and..")
+        self.clean_bathroom(time)
+#Robotica=CookRobot()
+#time=int(input("At what hour did you arrive home?"))
+#Robotica.cook(time)
+
+#Task4 create a super robot that can clean, move, bark, and play games
+class SuperRobot():
+    def __init__(self,name):
+        self.first=Robot()
+        self.second=Dog()
+        self.third=CleanRobot()
+        self.forth=CookRobot()
+        self.name=name
+    def playGame(self):
+        print("alphago game")
+    def move(self):
+        return self.first.move() #using robot class method
+    def bark(self):
+        return self.second.bark() #using dog class method
+    def clean(self):
+        return self.third.clean() #using cleanrobot class method
+    def move_cook(self):
+        self.move() 
+        return self.forth.cook(time)
+
+machineDog=SuperRobot(Robotica)
+#machineDog.move()
+#machineDog.bark()
+#machineDog.playGame()
+time=int(input("At what hour did you arrive home?"))
+machineDog.move_cook()
