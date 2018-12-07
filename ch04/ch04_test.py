e63145d70f684eb4a1203b1fbdc220ce0ae574b9
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-07 16:40:33 +0000

    did ex30 and 31 from LPTHW in ch04

---
+++ ch04/ch04_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 16:02:55 2018
+
+@author: maria
+"""
+from ch04_functions import *
+
+#############################ex30 from LPTHW
+print("Exercise 30 from LPTHW: ")
+people=30
+cars=40
+buses=15
+decidePeopleOrBusesOrCars()
+print("\n")
+############################ex 31 from LPTHW
+print("Exercise 30 from LPTHW: ")
+goInDarkRoomGame()  
