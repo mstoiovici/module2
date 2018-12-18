f434440b39dd6b0c9f156196e483c431ae22d32e
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-18 16:54:19 +0000

    cleared folder ch01

--- ch01_intro/learn_python_the_hard_way.py
+++ ch01_intro/learn_python_the_hard_way.py
@@ -6,48 +6,49 @@ Created on Wed Nov 28 16:56:28 2018
 @author: maria
 """
 print("--------------------------task1-----------------------")
-#print("Hello World!")
-#print("Hello Again")
-#print("I like typing this.")
-#print("This is fun")
-#print("Yay!Printing.")
-#print("I'd much rather you 'not'.")
-#print('I "said" do not touch this.')
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print("Yay!Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
 
 
 print()
 print("--------------------------task2-----------------------")
-#hi = "hello there"
-#name = "ana"
-#greet = hi + name  
-#print(greet)
-#greeting = hi + " " + name
-#print(greeting)
-#silly = hi + (" " + name)*3
-#print(silly)
+hi = "hello there"
+name = "ana"
+greet = hi + name  
+print(greet)
+greeting = hi + " " + name
+print(greeting)
+silly = hi + (" " + name)*3
+print(silly)
 
 
 
 print()
 print("--------------------------task3-----------------------")
-#x = 1
-#print(x)
-#x_str = str(x)
-#print("my fav number is", x, ".","x=", x)
-#print("my fav number is", x_str + "." + "x=" + x_str)
-##print("my fav number is" + x_str + "." + "x=" + x_str)
+x = 1
+print(x)
+x_str = str(x)
+print("my fav number is", x, ".","x=", x)
+print("my fav number is", x_str + "." + "x=" + x_str)
+#print("my fav number is" + x_str + "." + "x=" + x_str)
 
 
 print()
 print("--------------------------task4-----------------------")
-#n=0
-#while n<5:
-#    print(n)
-#    n=n+1
+n=0
+while n<5:
+    print(n)
+    n=n+1
 
-#for n in range(5):
-#    print(n)
+for n in range(5):
+    print(n)
 mysum=0
+
 for i in range(7,10):
     mysum +=1
 print(mysum)
