bdf044349cb7952688f6e3a07e93821e3dd1b12e
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-18 16:38:23 +0000

    reorganized ch01

--- ch01_intro/ch1_Mariana.py
+++ ch01_intro/ch1_Mariana.py
@@ -6,6 +6,7 @@ Created on Wed Nov 28 14:30:06 2018
 @author: maria
 """
 
+print("--------------task1----------------------------------")
 A=2 + 3
 B=2.9 + 3.2
 C=(3.2 / 2.0) + 7
@@ -14,20 +15,71 @@ E=4**(1/2)
 F=str(6) + "a"
 print(A)
 print(B)
+
+
+print()
+print("--------------task2----------------------------------")
 age=5
 #age="almost three"
 a_longer_name="hello,CFG!"
 print(age)
 print(a_longer_name)
 print(age)
+
+print()
+print("--------------task3----------------------------------")
 print("Bob"*3)
 #print("Bob"+3)
+
+print()
+print("--------------task4----------------------------------")
 print("hello".upper())
 print("GOODBYE".lower())
 print("the lord of the rings".title())
+
+
+print()
+print("--------------task5----------------------------------")
 age=5
 like="painting"
 age_description="My age is {} and I like {}.".format(age,like)
 print(age_description) #this prints the variable age_description
+
+
+print()
+print("--------------task6----------------------------------")
 print(10/3)
 print(10%3)
+
+
+print()
+print("--------------task7----------------------------------")
+a=1
+a=a+1
+print(a)
+
+print()
+print("--------------task8----------------------------------")
+b="hello"
+print(b)
+c=b.title()
+print(b)
+print(c)
+
+
+print()
+print("--------------task9----------------------------------")
+d="hello"
+e=d.title()
+print(d)
+print(e)
+
+print()
+print("--------------task10----------------------------------")
+name="Dave"
+f="Hello {0}!".format(name)
+print(f)
+name="Sarah"
+print(f)
+print(name)
+print(name*5)
