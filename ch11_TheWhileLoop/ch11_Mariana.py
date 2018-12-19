37887dfeae18240c5bbe186d7d1b5a87ab80cd14
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-19 15:05:36 +0000

    moved guessing game tasks to ch10_Mariana

--- ch11_TheWhileLoop/ch11_Mariana.py
+++ ch11_TheWhileLoop/ch11_Mariana.py
@@ -92,5 +92,87 @@ def give_greetings(greeting_no):
 greeting_no=int(input("How many greeting would you like to give?"))
 give_greetings(greeting_no)
 
+print("----Task6 Guessing game 1------------------------------------------")
+from random import randint
+def guess(attempts,end_range):
+    
+    number=randint(1,end_range)
+    print("Welcome! Can you guess my secret number?\n")
+    
+    while attempts>0:
+        answer=int(input("So, what's the number?\n"))
+        if answer==number:
+            print("You guessed! You won!")
+            break
+        else:
+            if answer<number:
+                print("No, that's too low!")
+            else:
+                print("No, that's too high!")
+            
+        attempts-=1
+        print("You have",attempts,"attempts left")
+    print("End-Of-Game: thanks for playing! ")
+
+guess(3,20)
+
+
+
+print()
+print("----Task7 Guessing dicing game - improved version------------------------------")
+
+#from random import randint
+def guess():
+    game=input("Do you want to play? Choose yes or no:\n")
+    while game=="yes":
+        guess=input("Do you think it will be odd or even?\n")
+        dice1=randint(1,6)
+        dice2=randint(1,6)
+        sum=dice1+dice2
+        print("First dice was: ",dice1,"and second dice was: ",dice2)
+        if (sum%2==0 and guess=="even")or(sum%2!=0 and guess=="odd"):
+            print("You guessed!")
+        else:
+            print("You din not guess!")
+        game_update=input("Do you still want to play? Choose yes or no:\n")
+        if game_update=="no":
+            break
+    print("Thanks for playing! See you later!")
+    
+guess()
+
+
+print()
+print("----Task7 Guessing dicing game - first version------------------------------------------")
+#from random import randint
+def guess():
+    game=input("Do you want to play? Choose yes or no:\n")
+    while game=="yes":
+        guess=input("Do you think it will be odd or even?\n")
+        dice1=randint(1,6)
+        dice2=randint(1,6)
+        sum=dice1+dice2
+        if sum%2==0 and guess=="even":
+            print("You guessed!")
+            print("First dice was: ",dice1,"and second dice was: ",dice2,"so the sum is even")
+        elif sum%2==0 and guess!="even":
+            print("You din not guess!")
+            print("First dice was: ",dice1,"and second dice was: ",dice2,"so the sum is even")
+
+        elif sum%2!=0 and guess=="odd":
+            print("You guessed!")
+            print("First dice was: ",dice1,"and second dice was: ",dice2,"so the sum is odd")
+
+        else:
+            print("You din not guess!")
+            print("First dice was: ",dice1,"and second dice was: ",dice2,"so the sum is odd")
+
+        game_update=input("Do you still want to play? Choose yes or no:\n")
+        if game_update=="no":
+            break
+    print("Thanks for playing! See you later!")
+    
+guess()
+
 
 
