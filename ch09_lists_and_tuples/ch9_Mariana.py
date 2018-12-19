319b4b1c0ff1430112a8ab9a01615ec48df88d8b
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-19 11:41:58 +0000

    cleared ch09

--- ch09_lists_and_tuples/ch9_Mariana.py
+++ ch09_lists_and_tuples/ch9_Mariana.py
@@ -5,9 +5,12 @@ Created on Thu Dec 13 09:24:19 2018
 @author: maria
 """
 
-###########Compound Data Types
-###########Lists and Tuples
-######################Lists
+print("------------------------Compound Data Types----------------")
+print("---------Lists and Tuples------------------------------")
+print("---------Lists------------------------------")
+
+print()
+print("---------task1 Write a list and access it's elements------------------------------")
 my_favourite_fruits=["apple","orange","banana"]
 x=["this",55,"that",my_favourite_fruits]
 print(x[0])
@@ -15,7 +18,15 @@ print(x[1])
 print(x[2])
 print(x[3])
 print(x[3][0]) ###### so you can access an element inside another element
+
+
+print()
+print("---------task2 Accessing elements out of range------------------------------")
 #print(x[4]) # error: list index out of range
+print("error: list index out of range")
+
+print()
+print("---------task3 Accessing elements------------------------------")
 print(x[-1])
 print(x[-2])  #### so you can access the first element of a list with 0, and so on from there. And you can access the last item of the list starting with -1 and then the previous last with -2 and so on.
 ########## x=[a,b,c,d]
@@ -28,17 +39,33 @@ print(x[-2])  #### so you can access the first element of a list with 0, and so 
 ########## x[-2]=c
 ########## x[-3]=b
 ########## x[-4]=a
+
+
+print()
 print(x[-1][-2])
 print(x[-2][-2]) # you can access a string as well and retrieve a part of it 
 print(x[-1][-2][-2][0]) #???????????????????????????????
 
+
+print()
+print("---------task4 Remove an element from a list------------------------------")
 x.remove(my_favourite_fruits) # removes the element from x
 print(x)
+
+print()
+print("---------task5 Modify an element from a list------------------------------")
 x[1]="and" # changes the element at position 1 with a new element:"and"
 print(x)
+
+
+print()
+print("---------task6 Append an element to a list------------------------------")
 x.append("again") # appends an element to a list
 print(x)
 
+
+print()
+print("---------task7 Operations on lists------------------------------")
 x=["the","cat","sat"]
 y=["on","the","mat"]
 z=x+y
@@ -47,6 +74,10 @@ set(x+z)
 print(set(x+z))## does something # set returns something but doesn't change the value of z. The return result of set is a dictionary.
 #w=(z-y) #this doesn't work. Minus is not suported for lists
 #print(w)
+
+
+print()
+print("---------task8 Slicing a list------------------------------")
 x=["this","and","that","once","again"]
 print(x[1:4])
 print(x[1:3])
@@ -54,6 +85,8 @@ print(x[3:5])
 print(x[-3:-1])
 
 
+print()
+print("---------task9 Sorting a list using sorted------------------------------")
 x=[7,11,3,9,2]
 print(x)
 print(sorted(x))
@@ -62,7 +95,10 @@ sorted(x,reverse=True)
 
 ####################from here is from pythontutor
 
-print("-----------------variable holding strings versus variable holding lists--------------")
+
+print()
+print("-----------------task 10 Variable holding strings versus variable holding lists--------------")
+print("--------------Variables")
 x="Mari"
 y=x
 y="Mariana"
@@ -70,6 +106,7 @@ print(y)
 print(x)
 
 
+print()
 x="Mari"
 y=x
 y="Mari"
@@ -77,15 +114,17 @@ print(y)
 print(x)
 
 
+
+print()
+print("----------------Lists")
 x=["Mari","Ana"]
 y=x
 y=["Mari","Ana","Maria"]
 print(y)
 print(x)
 
-
-print("-----------------operations on lists--------------")
-
+print()
+print("-----------------Task 11 Operations on lists--------------")
 x=["the","cat","sat"]
 y=["on","the","mat"]
 z=x+y
@@ -95,79 +134,102 @@ print(set(x+z))
 #w=(z-y)
 #print(w)
 
-print("-----------------sort versus sorted on lists--------------")
+
+print()
+print("-----------------Task 12 Sort versus sorted on lists--------------")
+print("----------------Sorted")
 x=[7,11,3,9,2]
 y=sorted(x)
 print(y)
+
+print("----------------Sort")
 x.sort()
 print(x)
 
+print("----------------Sorted")
 x=["this","that","again","and","if"]
 y=sorted(x)
 print(y)
+
+print("----------------Sort")
 x.sort()
 print(x)
 print(x.sort())
 
-print("-----------------using reverse on sort and sorted--------------")
+print()
+print("-----------------Task 13 Using reverse with sort and sorted--------------")
+print("----------------Sorted")
 x=[7,11,3,9,2]
 print(sorted(x))
 print(x)
 print(sorted(x,reverse=True))
 print(x)
 
+
+print()
+print("----------------Sort")
 x=[7,11,3,9,2]
 x.sort()
 print(x)
 x.sort(reverse=True)
 print(x)
 
-print("-----------------Using sort () method and generic sorted() functions on lists holding strings--------------")
 print()
-print("-----------------generic sorted() function--------------")
+print("----------------Sorted")
 x=["ab","cs","yw","zs","hd"]
 y= sorted(x,reverse=True)
 print("now x is: ",x)
 print("now y is: ",y)
+
+
 print()
-print("-----------------object method .sort()--------------")
+print("----------------Sort")
 x.sort(reverse=True)
 y=x.sort(reverse=True)
 print("now x is: ",x)
 print("now y is: ",y)
-print()
 
-print("------------Lists versus tuples with del function--------------")
+
+print()
+print("----------------------------Tuples------------------------------------------")
+print("------------Task1 Lists versus tuples with del function--------------")
+print("----list--")
 a=[0,1,2,3,4,5,6,7,8,9]
 #del a[-1]
 del a[-4]
 
+print("----tuple--")
 b=(0,1,2,3,4,5,6,7,8,9)
 #del b[-1] # doesn't work cause tuple doesn't support the del function, or other changes to the tuple changes
 
-print("------------lists versus tuple with new assigning-----------")
+
+print("------------Task2 lists versus tuple with new assigning-----------")
+print("----list--")
 a=[0,1,2,3,4,5,6,7,8,9]
 a[0]=50
 print(a)
 
+print("----tuple--")
 b=(0,1,2,3,4,5,6,7,8,9)
 #b[0]=50 # tuples doesn't support new assignements to their elements, tuples are immutable
 
-print("------------lists versus tuple with append-----------")
+print("------------Task3 Lists versus tuple with append-----------")
+print("----list--")
 a=[0,1,2,3,4,5,6,7,8,9]
 a.append("z")
 print(a)
 
+print("----tuple--")
 b=(0,1,2,3,4,5,6,7,8,9)
 #b.append("z") # tuple doesn't support append attribute/methos, is imumtable
 print(b)
 
-print("------------changing tuples into lists-----------")
+print("------------Task4 Changing tuples into lists-----------")
 b=(0,1,2,3,4,5,6,7,8,9)
 list(b) # to change the tuple in a list
 y=list(b) #to assign the value to a new variable cause b didn't change, it's still a tuple
 print(type(b))
 print(type(y))
-"""
+
 
 
--- ch09_lists_and_tuples/lambda_function.py
+++ ch09_lists_and_tuples/lambda_function.py
@@ -4,34 +4,50 @@ Created on Thu Dec 13 13:47:59 2018
 
 @author: maria
 """
-"""
+print("------------------------Lambda function----------------")
+print("------Task1 Lists vs tuples----------------")
 a=[50,1,2,3,4,5,6,7,8,9]
 b=(10,1,2,3,4,5,6,7,8,9)
 my_favourite_fruits=["apple","orange","banana"]
 x=["yw","zs","cs","hd","ab"]
 y=["ab","cs","hd","yw","zs"]
 z=["zs","yw","hd","cs","ab"]
-"""
-"""
+print(a)
+print(b)
+print(my_favourite_fruits)
+print(x)
+print(y)
+print(z)
+
+print()
+print("-----Task2 Lists vs tuples when using simple sorted and sorted with lambda function-------")
+print("------Simple sorted----------------")
 a = [0,1,2,3,4,5,6,7,8,9]
 b = (0,1,2,3,4,5,6,7,8,9)
 myFavF = ["apple", "orange", "banana"]
 x = ["xa", "sb", "lf", "hw", "ed", "fy"]
 y = sorted(x)
+
+print()
 z = ["fg", "uj", "sx", "uj", "ww", "cf"]
 x2=[("a",3,z),("c",1,y),("b",5,x)]
 print(x2)
 print()
 print(sorted(x2))
-print()
 
+
+
+print()
+print("------And with lambda function----------------")
 print(sorted(x2,key=lambda s:s[1]))
 print()
 print(sorted(x2,key=lambda s:s[2]))
 print()
 print(sorted(x2,key=lambda s:s[2][1]))
-"""
-"""
+
+
+print()
+print("------Task3----------------")
 x=[1,2,3,4]
 y=[3,4,10,3]
 z=[20,10,30,40]
@@ -39,8 +55,9 @@ x2=[("a",3,z),("c",1,y),("b",5,x)]
 print(sorted(x2,key=lambda s:s[2]))
 print(sorted(x2,key=lambda s:s[2][0]))
 #print(sorted(x2,key=lambda s:s[2][-1]))
-"""
-"""
+
+print()
+print("------Task4----------------")
 a = [0,1,2,3]
 b = (0,1,2,3)
 myFavF = ["apple", "orange"]
@@ -54,7 +71,10 @@ print(sorted(x2))
 print(sorted(x2,key=lambda s:s[1]))
 print(sorted(x2,key=lambda s:s[2]))
 print(sorted(x2,key=lambda s:s[2][1]))
-"""
+
+
+print()
+print("------Task5----------------")
 a = [0,1,2,3]
 b =(4,5,6)
 myFavF = ["Joke", "Chen"]
@@ -63,10 +83,10 @@ y = sorted(x)
 z = ["Jennifer", "Katie", "Muna"]
 x2=[("c",75,z),("a",23,y),("b",111,x)]
 print(x2)
-#print(sorted(x2))
-#print()
-#print(sorted(x2,key=lambda s:s[1]))
-#print(sorted(x2,key=lambda s:s[1])[0])
-#print(sorted(x2,key=lambda s:s[2]))
-#print(sorted(x2,key=lambda s:s[2][1]))
-#print(sorted(x2,key=lambda s:s[2][-2][-2]))
+print(sorted(x2))
+print()
+print(sorted(x2,key=lambda s:s[1]))
+print(sorted(x2,key=lambda s:s[1])[0])
+print(sorted(x2,key=lambda s:s[2]))
+print(sorted(x2,key=lambda s:s[2][1]))
+print(sorted(x2,key=lambda s:s[2][-2][-2]))
